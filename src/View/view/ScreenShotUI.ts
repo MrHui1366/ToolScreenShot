5d1346b72063c76311af50d742dbd0209859c40a
zhangxianghui123456 <717077505@qq.com>
2021-04-28 16:11:19 +0800

    优化

--- src/View/view/ScreenShotUI.ts
+++ src/View/view/ScreenShotUI.ts
@@ -10,12 +10,29 @@ import { ui } from "src/ui/layaMaxUI";
  */
 export default class ScreenShotUI extends ui.ViewFile.ScreenShotUIUI {
 
-    /** List数据源 */
-    private listAtt: FashionShopTableData[];
+    /** 头发列表 */
+    private hairList: FashionShopTableData[];
 
-    private manList: FashionShopTableData[];
+    /** 衣服列表 */
+    private clothesList: FashionShopTableData[];
 
-    private womanList: FashionShopTableData[];
+    /** 鞋子列表 */
+    private shoesList: FashionShopTableData[];
+
+    /** 表情列表 */
+    private faceList: FashionShopTableData[];
+
+    /** 头发下标 */
+    private hariIndex: number = 0;
+
+    /** 衣服下标 */
+    private clothesIndex: number = 0;
+
+    /** 鞋子下标 */
+    private shoesIndex: number = 0;
+
+    /** 表情下标 */
+    private faceIndex: number = 0;
 
     /** 穿戴服装数据 */
     private clothing: Array<number>;
@@ -31,8 +48,7 @@ export default class ScreenShotUI extends ui.ViewFile.ScreenShotUIUI {
     }
 
     onEnable() {
-        this.manList = FashionShopDataManager.getInstance().getListByGender( 1 );
-        this.womanList = FashionShopDataManager.getInstance().getListByGender( 2 );
+        this.initData();
         this.btn_start.on( Laya.Event.CLICK, this, this.startBtn );
         this.btn_stop.on( Laya.Event.CLICK, this, this.stopBtn );
     }
@@ -43,24 +59,42 @@ export default class ScreenShotUI extends ui.ViewFile.ScreenShotUIUI {
 
     /** 初始化数据 */
     private initData() {
-
-
+        this.hairList = [];
+        this.clothesList = [];
+        this.shoesList = [];
+        this.faceList = [];
+        let tableData = FashionShopDataManager.getInstance().getListByGender( this.nowGender );
+        this.hairList = eleectPar( 0, tableData );
+        this.clothesList = eleectPar( 1, tableData );
+        this.shoesList = eleectPar( 2, tableData );
+        this.faceList = eleectPar( 3, tableData );
+        function eleectPar( index: number, data: FashionShopTableData[] ) {
+            let list: FashionShopTableData[] = new Array();
+            for ( let i = 0; i < data.length; i++ ) {
+                if ( data[ i ].type == index ) list.push( data[ i ] );
+            }
+            return list;
+        }
     }
 
     /** 初始化 */
     private init() {
-        this.clothing = [ 0, 1, 2, 3 ];
-        RoleManager.getInstance().createRole( this.role_root, this.nowGender, this.clothing );
+        let hariID = this.hairList[ this.hariIndex ].id;
+        let clothesID = this.clothesList[ this.clothesIndex ].id;
+        let shoesID = this.shoesList[ this.shoesIndex ].id;
+        let faceID = this.faceList[ this.faceIndex ].id;
+        this.clothing = [ hariID, clothesID, shoesID, faceID ];
+        this.Role = RoleManager.getInstance().createRole( this.role_root, this.nowGender, this.clothing );
+        this.showFashion();
     }
 
     /** 显示当前穿着的衣服 */
     private showFashion() {
-        let tableData = FashionShopDataManager.getInstance().getDataById( this.clothing[ 0 ] );
-        this.img_hair.skin = '';
-        this.img_clothes.skin = '';
-        this.img_shoes.skin = '';
-        this.img_face.skin = '';
-
+        let _gender: string = this.nowGender == 1 ? 'M' : 'W';
+        this.img_hair.skin = 'FashionShop/img_hair_' + _gender + this.hairList[ this.hariIndex ].level + '.png';
+        this.img_clothes.skin = 'FashionShop/img_clothes_' + _gender + this.clothesList[ this.clothesIndex ].level + '.png';
+        this.img_shoes.skin = 'FashionShop/img_shoe_' + _gender + this.shoesList[ this.shoesIndex ].level + '.png';
+        this.img_face.skin = 'FashionShop/img_face_' + _gender + this.faceList[ this.faceIndex ].level + '.png';
     }
 
     /** 换装 */
